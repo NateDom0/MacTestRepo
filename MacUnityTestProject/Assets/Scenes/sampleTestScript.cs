19b3d31b138841dcb10d5ee36343ab6a8b40952e
NateDom0 <nathanielrd98@gmail.com>
2022-05-08 15:08:08 -0700

    Edit Mac Unity test project
    Edited gitignore, added object and test script in Unity project.

---
+++ MacUnityTestProject/Assets/Scenes/sampleTestScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//test comment 5/8/22
+
+public class sampleTestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
